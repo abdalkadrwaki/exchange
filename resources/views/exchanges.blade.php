0b39afc4b1545362537d8e7b0c13deeeffcccaf5
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-05-21 18:25:24 +0300

    Update exchanges.blade.php

--- resources/views/exchanges.blade.php
+++ resources/views/exchanges.blade.php
@@ -24,53 +24,56 @@
     <div class="container" style="">
         <!-- بطاقة نموذج التحديد -->
         <!-- بطاقة الفلترة -->
-        <div class="card mb-4" style="max-width: 1200px; margin: auto; margin-top: 39px;">
-            <div class="card-header text-blue-600">
-                <h2 class="mb-1 text-center font-bold text-xl">استلام/ تسليم</h2>
+         <div class="card mb-4" style="max-width: 1200px; margin: auto; margin-top: 39px;">
+            <div class="card-header      text-blue-600">
+                <h2 class="mb-1 text-center font-bold text-xl ">استلام/ تسليم</h2>
             </div>
             <div class="card-body">
-                <div class="p-4 bg-custom-gray2 shadow-md rounded-md">
-                    <form action="{{ route('Exchanges.filter') }}" method="GET">
+                <div class="p-4 bg-custom-gray2 shadow-md rounded-md ">
+                    <!-- نموذج تحديد النطاق الزمني ونوع العملة -->
+                    <form action="{{ route('deliveries.filter') }}" method="GET">
                         <div class="row">
                             <div class="col-md-4 mb-3">
                                 <label for="from_date">من تاريخ</label>
                                 <input type="date" id="from_date" name="from_date" class="form-control"
-                                    value="{{ $fromDate }}">
+                                    value="{{ request('from_date') ?? date('Y-m-d') }}">
                             </div>
                             <div class="col-md-4 mb-3">
                                 <label for="to_date">إلى تاريخ</label>
                                 <input type="date" id="to_date" name="to_date" class="form-control"
-                                    value="{{ $toDate }}">
+                                    value="{{ request('to_date') ?? date('Y-m-d') }}">
                             </div>
-
                             <div class="col-md-4 mb-3">
                                 <label for="currency">العملة</label>
-                                <select id="currency" name="currency" class="form-select scrollable-input right-64">
+                                <select id="currency" name="currency" class="form-select  scrollable-input right-64 ">
 
                                     <option value="all" {{ request('currency') == 'all' ? 'selected' : '' }}>الكل
                                     </option>
                                     <option value="SYP" {{ request('currency') == 'SYP' ? 'selected' : '' }}>سوري
                                     </option>
                                     <option value="USD" {{ request('currency') == 'USD' ? 'selected' : '' }}>دولار
+                                        أمريكي
                                     </option>
                                     <option value="TRY" {{ request('currency') == 'TRY' ? 'selected' : '' }}>تركي
                                     </option>
                                     <option value="EUR" {{ request('currency') == 'EUR' ? 'selected' : '' }}>يورو
                                     </option>
                                     <option value="SAR" {{ request('currency') == 'SAR' ? 'selected' : '' }}>ريال
-                                        سعودي</option>
+                                        سعودي
+                                    </option>
                                 </select>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col text-center">
-                                <button type="submit" class="btn btn-primary w-full text-xl">تنفيذ</button>
+                                <button type="submit" class="btn  btn-primary  w-full text-xl">تنفيذ </button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+
 
         <!-- بطاقة الجدول -->
         <div class="card" style="max-width: 1200px; margin: auto;">
@@ -357,7 +360,7 @@
 
 
 
-  
+
 
     </div>
 
