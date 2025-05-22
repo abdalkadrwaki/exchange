6b2be639c8150855d0c1c467280b2133a347b057
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-05-22 13:04:42 +0300

    Update exchanges.blade.php

--- resources/views/exchanges.blade.php
+++ resources/views/exchanges.blade.php
@@ -65,6 +65,13 @@
                 </div>
             </div>
         </div>
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        @if (session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+        @endif
 
         <!-- بطاقة الجدول -->
         <div class="card" style="max-width: 1200px; margin: auto;">
