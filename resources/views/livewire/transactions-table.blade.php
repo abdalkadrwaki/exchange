2a72e8f23af05d5195747315224a546cb14120b5
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-05-24 13:46:47 +0300

    Update transactions-table.blade.php

--- resources/views/livewire/transactions-table.blade.php
+++ resources/views/livewire/transactions-table.blade.php
@@ -101,22 +101,17 @@
             </thead>
             <tbody>
                 @foreach ($transactions as $transaction)
-            @php
-    $typeValue = $tran[$transaction['type']] ?? $transaction['type'];
-    $transactionType = $tran[$transaction['transaction_type']] ?? $transaction['transaction_type'];
+                    @php
+                        $typeValue = $tran[$transaction['type']] ?? $transaction['type'];
+                        $transactionType = $tran[$transaction['transaction_type']] ?? $transaction['transaction_type'];
 
-    $rowClass = match (true) {
-        // إذا كانت العملية صرافة وبيع دولار
-        $transaction['type'] === 'Exchange' && $transaction['transaction_type'] === 'sell' && $transaction['currency_name3'] === 'USD' => 'table-danger',
-        // إذا كانت العملية صرافة وشراء دولار
-        $transaction['type'] === 'Exchange' && $transaction['transaction_type'] === 'buy' && $transaction['currency_name'] === 'USD' => 'table-success',
-        // الشروط الحالية
-        $typeValue === 'صرافة' => 'table-warning',
-        $transactionType === 'تسليم' => 'table-danger',
-        $transactionType === 'استلام' => 'table-success',
-        default => '',
-    };
-@endphp
+                        $rowClass = match (true) {
+                            $typeValue === 'صرافة' => 'table-warning',
+                            $transactionType === 'تسليم' => 'table-danger',
+                            $transactionType === 'استلام' => 'table-success',
+                            default => '',
+                        };
+                    @endphp
 
                     <tr class="{{ $rowClass }}">
 
