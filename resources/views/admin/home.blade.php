8541739944dcc7559babb5ec12ad059f6ca7adb2
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-05-31 15:11:15 +0300

    exchange

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,13 +8,14 @@
     <div class="py-6">
 
         <div class="container mx-auto px-4 mt-8 ">
-      <livewire:exchange-difference-boxes />
+            <livewire:exchange-difference-boxes />
 
-                <div class="max-w-12x1 mx-auto bg-white shadow rounded-md p-4 mt-3 ">
+            <div class="max-w-12x1 mx-auto bg-white shadow rounded-md p-4 mt-3 ">
 
 
-                    @livewire('transactions-table')
-                </div>
+                @livewire('transactions-table')
+            </div>
 
         </div>
+    </div>
 </x-app-layout>
